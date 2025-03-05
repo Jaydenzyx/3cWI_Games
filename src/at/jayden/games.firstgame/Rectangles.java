eb239a3833c1e9284844579f3ea39e42fce96fd0
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2025-03-05 09:32:59 +0100

    firstgame_finished

---
+++ src/at/jayden/games.firstgame/Rectangles.java
@@ -0,0 +1,53 @@
+package at.jayden.games.firstgame;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.tests.AnimationTest;
+
+public class Rectangles extends BasicGame {
+    public Rectangles(String title) {
+        super(title);
+    }
+
+    private float x;
+    private float y;
+    private float speed;
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+    this.x = 100;
+    this.speed = 10.0f;
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+    this.x += (float) delta/this.speed;
+    this.y ++;
+
+
+
+    if(this.x > 800){
+        this.x = 0;
+        this.y = 0;
+    }else if(this.y>800){
+        this.y = 0;
+    }
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        graphics.drawRect(this.x,this.y,100,100);
+        graphics.drawString("Hello",130,130);
+    }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+            container.setDisplayMode(800,600,false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
